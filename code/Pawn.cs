470456100eab4895995c047b2bb870727eb5cc48
J319s0 <josh.smith3190@gmail.com>
2022-12-16 12:57:38 +0800

    camera clamp + fixed position
    player now in fixed position next to lake. player camera clamped on y axis -70-70 degrees

--- code/Pawn.cs
+++ code/Pawn.cs
@@ -24,17 +24,20 @@ partial class Pawn : AnimatedEntity
 	}
 
 	// An example BuildInput method within a player's Pawn class.
-	[ClientInput] public Vector3 InputDirection { get; protected set; }
 	[ClientInput] public Angles ViewAngles { get; set; }
 
 	public override void BuildInput()
 	{
-		InputDirection = Input.AnalogMove;
-
 		var look = Input.AnalogLook;
+
+		if(ViewAngles.pitch is > 90 or < -90)
+		{
+			look = look.WithYaw(look.yaw * -1f);
+		}
 
 		var viewAngles = ViewAngles;
 		viewAngles += look;
+		viewAngles.pitch = viewAngles.pitch.Clamp(-70,70);
 		ViewAngles = viewAngles.Normal;
 	}
 
@@ -44,26 +47,7 @@ partial class Pawn : AnimatedEntity
 	public override void Simulate( IClient cl )
 	{
 		base.Simulate( cl );
-
 		Rotation = ViewAngles.ToRotation();
-
-		// build movement from the input values
-		var movement = InputDirection.Normal;
-
-		// rotate it to the direction we're facing
-		Velocity = Rotation * movement;
-
-		// apply some speed to it
-		Velocity *= Input.Down( InputButton.Run ) ? 1000 : 200;
-
-		// apply it to our position using MoveHelper, which handles collision
-		// detection and sliding across surfaces for us
-		MoveHelper helper = new MoveHelper( Position, Velocity );
-		helper.Trace = helper.Trace.Size( 16 );
-		if ( helper.TryMove( Time.Delta ) > 0 )
-		{
-			Position = helper.Position;
-		}
 
 		// If we're running serverside and Attack1 was just pressed, spawn a ragdoll
 		if ( Game.IsServer && Input.Pressed( InputButton.PrimaryAttack ) )
